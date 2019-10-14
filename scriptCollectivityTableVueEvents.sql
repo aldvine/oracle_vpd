a11e834ae9f93c42729227dc7ba5802bf5280a55
aldvine <aldvine.blanchard@gmail.com>
2019-10-14 09:56:02 +0200

    suppression fichiers inutiles

--- scriptCollectivityTableVueEvents.sql
+++
@@ -1,7 +0,0 @@
-/** USER4 **/
-EXECUTE ADMIN18.set_user_ctx_pkg.set_user;
-/** Affichage du role */
-SELECT SYS_CONTEXT('USER_CTX','role') FROM DUAL;
-
-/** Affiche les éléments de la vue */
-SELECT * FROM ADMIN18.VUE_EVENTS WHERE idEvent = 'EVENT1';
--- user_script.sql
+++
@@ -1,1 +0,0 @@
-EXECUTE ADMIN18.set_user_ctx_pkg.set_user;
